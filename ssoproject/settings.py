ece6a14eca06615154c45b8dcca80686176c7e60
rakeshimpinge <harpreet.impinge@gmail.com>
2020-07-10 22:24:22 +0530

    changed python version

--- ssoproject/settings.py
+++ ssoproject/settings.py
@@ -210,6 +210,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+heroku config:set DISABLE_COLLECTSTATIC=1
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
