ce29890f2cfe8947b88eb4a0db0795afee7a9e33
Manish <gargmani312@gmail.com>
2020-07-14 18:02:00 +0530

    updated settinfs.py file

--- ssoproject/settings.py
+++ ssoproject/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ]
 
 AUTH_USER_MODEL = 'sso.User'
